8ec0b69df76c29c6ee65a9c755e6b3447017f31e
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-17 17:13:26 +0200

    display the moves

--- index.php
+++ index.php
@@ -10,8 +10,10 @@
 <body>
 <form action="">
     <p>Pokemon Name or Id:</p>
+    <label for="">
     <input type="text" name="search">
     <button>Search</button>
+    </label>
 </form>
 <?php
 
@@ -26,15 +28,19 @@ if (!empty($_GET["search"])){ // input value
     $poke_name = $poke_array["name"];
     $poke_Id = $poke_array["id"];
     $search = $_GET['search'];
-    echo "Name: {$poke_name} <br> ID: {$poke_Id} <br>";
+    echo "Name of Pokemon: {$poke_name} <br> ID: {$poke_Id} <br>";
     // get the the pic of the pokemon
-    $poke_pic = $poke_array["sprites"]["front_default"];
+    $poke_pic = $poke_array["sprites"]["front_default"]; // pic from front
+    $poke_pic2 = $poke_array["sprites"]["back_shiny"];// pic from back
     echo "<img src='" . $poke_pic . "' width=200>";
+    echo "<img src='" . $poke_pic2 . "' width=200> <br>";
     // get the pokemon moves
     $poke_moves = $poke_array['moves'];
-    $poke_move1 = $poke_moves[0];
-    $poke_move2 = $poke_moves[1];
-    echo "Move 1: {$poke_move1} <br> move 2: {$poke_move2}";
+    $poke_move1 = $poke_moves[0]['move']['name'];
+    $poke_move2 = $poke_moves[1]['move']['name'];
+    $poke_move3 = $poke_moves[2]['move']['name'];
+    $poke_move4 = $poke_moves[4]['move']['name'];
+    echo "Move 1: {$poke_move1} <br> Move 2: {$poke_move2} <br> Move 3: {$poke_move3} <br> Move 4: {$poke_move4}";
 
 } else if (empty($_GET["search"])){
     echo "You have to write a name or id.";
