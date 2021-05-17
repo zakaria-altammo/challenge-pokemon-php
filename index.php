c6b1f8ef2fd6fa00cc096835e4ac998d634fdcb8
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-17 16:53:45 +0200

    i displayed the pic of pokemon and working on displaying moves

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>challenge-pokemon-php</title>
 </head>
 <body>
+<form action="">
+    <p>Pokemon Name or Id:</p>
+    <input type="text" name="search">
+    <button>Search</button>
+</form>
 <?php
 
 // i use if  statement to check if the value of the input is empty or not
@@ -21,21 +26,22 @@ if (!empty($_GET["search"])){ // input value
     $poke_name = $poke_array["name"];
     $poke_Id = $poke_array["id"];
     $search = $_GET['search'];
-    echo "Name: {$poke_name} <br> ID: {$poke_Id}";
+    echo "Name: {$poke_name} <br> ID: {$poke_Id} <br>";
     // get the the pic of the pokemon
+    $poke_pic = $poke_array["sprites"]["front_default"];
+    echo "<img src='" . $poke_pic . "' width=200>";
+    // get the pokemon moves
+    $poke_moves = $poke_array['moves'];
+    $poke_move1 = $poke_moves[0];
+    $poke_move2 = $poke_moves[1];
+    echo "Move 1: {$poke_move1} <br> move 2: {$poke_move2}";
 
-    
 } else if (empty($_GET["search"])){
-    echo "You have to write a name or id";
+    echo "You have to write a name or id.";
 }
 
-
 ?>
-<form action="">
-    <p>Pokemon Name or Id</p>
-<input type="text" name="search">
-    <button>Search</button>
-</form>
+
 
 </body>
 </html>
