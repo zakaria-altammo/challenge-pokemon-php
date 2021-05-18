71204de3d41ec7eef8a3874310ec0886ebb9fb52
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-18 17:06:11 +0200

    add some style to the game

--- style.php
+++ style.php
@@ -4,36 +4,34 @@
 header("Content-type: text/css");
 
 
-$font_family = 'Arial, Helvetica, sans-serif';
-$font_size = '0.7em';
-$border = '1px solid';
 ?>
 
- * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+* {
+box-sizing: border-box;
+margin: 0;
+padding: 0;
 }
 body {
-    text-align: center;
-    background: url('DVMT.jpg');
-    background-size: cover;
+text-align: center;
+background: url('DVMT.jpg');
+background-size: cover;
 
 
 }
 h1 {
 text-shadow: 0 0 15px black;
+font-family: sans-serif;
+color: #CBAC88;
 
-    font-family: Algerian, sans-serif;
-    color: ;
+
 }
 div {
-    padding: 10px;
-    width: 350px;
-background-color: darkseagreen;
-    color: black;
-    position: absolute;
-    left: 465px;
+padding: 10px;
+width: 350px;
+background-color: #FCFFFC;
+color: #2D3A3A;
+position: absolute;
+left: 465px;
 border-radius: 10px;
 font-weight: bolder;
 box-shadow: 0 0 15px black;
@@ -41,26 +39,33 @@ box-shadow: 0 0 15px black;
 
 }
 input {
-    margin: 5px;
-    width: 350px;
-    height: 30px;
+margin: 5px;
+width: 350px;
+height: 30px;
+box-shadow: 0 0 15px black;
+border: none;
+
 }
 p {
-    font-family: Andalus, sans-serif;
-    font-size: 15px;
+font-family: Andalus, sans-serif;
+font-size: 15px;
 }
 button {
-    background-color: firebrick;
-    width: 110px;
-    margin: 0 0 7px 0;
-    height: 30px;
-    border-radius: 5px;
-}
+background-color: #CBAC88;
+width: 110px;
+margin: 0 0 7px 0;
+height: 30px;
+border-radius: 5px;
+border: none;
 
-img {
-    width: 150px;
-    height: 150px;
 }
-ul {
-list-style-type: none
+button:hover {
+background-color: #2BA84A;
+}
+button:active {
+transform: translateY(2px);
+}
+img {
+width: 125px;
+height: 125px;
 }
